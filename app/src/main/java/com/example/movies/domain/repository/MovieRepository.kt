a817d164f10d68c88bace33b7e0edb0aa432eb6e
Uday Vig <uday.vig@gmail.com>
2024-08-16 02:53:34 +0530

    Fix repository methods return type

--- app/src/main/java/com/example/movies/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/example/movies/domain/repository/MovieRepository.kt
@@ -3,7 +3,7 @@ package com.example.movies.domain.repository
 import com.example.movies.data.remote.dto.MovieDto
 
 interface MovieRepository {
-    suspend fun getTrendingMovies(): List<MovieDto>
+    suspend fun getTrendingMovies(): MovieDto
 
-    suspend fun getQueryMovies(query: String): List<MovieDto>
+    suspend fun getQueryMovies(query: String): MovieDto
 }
